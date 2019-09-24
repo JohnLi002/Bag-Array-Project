c72ce7ee6b071bbfcab7a1b48a0fc1cd9cfbe788
John Li <lij19@wit.edu>
2019-09-24 13:25:11 -0400

    added LinkedBag.java to PartB and used Linked Bag as a way to create the guessing game

--- src/partB/GuessingGame.java
+++ src/partB/GuessingGame.java
@@ -1,7 +1,70 @@
 package partB;
 
+import java.util.Scanner;
+
 public class GuessingGame {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		boolean keepGoing = true;
 		
+		while(keepGoing) {
+			boolean correct = false;
+			System.out.println("Input the amount of numbers you want to guess for.");
+			int userNum = input.nextInt();
+			
+			LinkedBag<Integer> randomNums = new LinkedBag<>();
+			for(int i = 0; i < userNum; i++) {
+				randomNums.add((int) (Math.random() * 10) + 1);
+			}
+			
+			//---------------
+			//used to show what is the current random number (for testing reasons
+			Object[] a = randomNums.toArray();
+			for(int i = 0; i < userNum; i++) {
+				System.out.println(a[i]);
+			}
+			//-------------
+			System.out.println("Enter " + userNum + " integars in the range from 1 to 10. Entries may be duplicate.");
+
+			while(!correct) {
+				System.out.println("Input guesses");
+				LinkedBag<Integer> userBag = new LinkedBag<>();
+				
+				for(int i = 0; i < userNum; i++) {
+					userBag.add(input.nextInt());
+				}
+				
+				if(randomNums.equals(userBag)) {
+					correct = true;
+				}
+				else {
+					int numCorrect = 0;
+					Object[] answerArray =  randomNums.toArray();
+					Object[] guessArray = userBag.toArray();
+					
+					for(int i = 0; i < userNum; i++) {
+						for(int ii = 0; ii < userNum; ii++) {
+							if(answerArray[ii].equals(guessArray[i])) {
+								numCorrect++;
+								System.out.println(answerArray[ii]);
+								answerArray[ii] = -1;
+								break;
+							}
+						}
+					}
+					System.out.println(numCorrect + " of your guesses are correct");
+				}
+			}
+			
+			System.out.println("You are correct! Play again?");
+			String contAnswer = input.next();
+			
+			if(contAnswer.toLowerCase().equals("no")) {
+				keepGoing = false;
+			}
+		}
+		
+		input.close();
+		System.out.println("Good Bye!");
 	}
 }
---
+++ src/partB/LinkedBag.java
@@ -0,0 +1,201 @@
+package partB;
+
+import java.util.Arrays;
+
+public class LinkedBag <T> implements BagInterface <T> {
+	private Node  firstNode;
+	private int numberOfEntries;
+	
+	public LinkedBag () {    // empty bag
+		firstNode = null;
+		numberOfEntries = 0;
+	}
+
+	@Override
+	public int getCurrentSize() {
+		return numberOfEntries;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		if (firstNode == null) {
+			assert (numberOfEntries == 0);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean add (T anEntry) {
+		Node newNode = new Node(anEntry);
+		newNode.next = firstNode;
+		firstNode = newNode;
+		numberOfEntries++;
+		return true;
+	}
+
+	@Override
+	public boolean remove(T anEntry) {
+		if(getCurrentSize() == 0) {
+			return false;
+		}
+		for (Node currentNode = firstNode; currentNode != null; currentNode = currentNode.next) {
+			if(currentNode.data.equals(anEntry) && getCurrentSize() == 1) {
+				firstNode = null;
+				numberOfEntries--;
+				return true;
+			} else if(currentNode.data.equals(anEntry)) {
+				currentNode.data = firstNode.data;
+				firstNode = firstNode.next;
+				numberOfEntries--;
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+	@Override
+	public T remove() {
+		if(firstNode.next != null) {
+			Node initial = firstNode;
+			firstNode = firstNode.next;
+			numberOfEntries--;
+			return initial.data;
+		}
+		else {
+			firstNode = null;
+		}
+		
+		return null;
+	}
+
+	@Override
+	public void clear() {
+		firstNode = null;
+		numberOfEntries = 0;
+	}
+
+	@Override
+	public boolean contains(T anEntry) {
+		Node check = firstNode;
+		while(check != null) {
+			if(check.equals(anEntry)) {
+				return false;
+			}
+			check = check.next;
+		}
+		return false;
+	}
+
+	@Override
+	public int getFrequencyOf(T anEntry) {
+		int counter = 0;
+		Node check = firstNode;
+		while(check != null) {
+			if(anEntry.equals(check.data)) {
+				counter++;
+			}
+			check = check.next;
+		}
+		return counter;
+	}
+
+	@Override
+	public T[] toArray() {
+		@SuppressWarnings("unchecked")
+		T[] result = (T[]) new Object[numberOfEntries];
+		int idx = 0;
+		for (Node currentNode = firstNode; currentNode != null; currentNode = currentNode.next) {
+			result[idx ++] = currentNode.data;
+		}
+		return result;
+	}
+
+
+	@Override
+	public T[] toArray(T[] input) {
+		T[] result = Arrays.copyOf(input, numberOfEntries);
+		int idx = 0;
+		for (Node currentNode = firstNode; currentNode != null; currentNode = currentNode.next) {
+			result[idx ++] = currentNode.data;
+		}
+		return result;
+	}
+	
+	public boolean equals(LinkedBag<T> other) {
+		if(other.getCurrentSize() != getCurrentSize()) {
+			return false;
+		}
+		
+		LinkedBag<T> given = new LinkedBag<>();
+		Node temp = other.firstNode;
+		
+		while(temp != null) {
+			given.add(temp.data);
+			temp = temp.next;
+		}
+		Node currentNode = firstNode;
+		while(currentNode != null) {
+			if(!given.remove(currentNode.data)) {
+				return false;
+			}
+			currentNode = currentNode.next;
+		}
+		
+		return true;
+	}
+	public LinkedBag<T> Union(LinkedBag<T> other) {
+		LinkedBag<T> result = new LinkedBag<T>();
+		Node temp = firstNode;
+		while(temp != null) {
+			result.add(temp.data);
+			temp = temp.next;
+		} 		temp = other.firstNode;
+		 while(temp != null) {
+			result.add(temp.data);
+			temp = temp.next;
+		}
+		
+		return result;
+	}
+	public LinkedBag<T> Intersection(LinkedBag<T> other) {
+		LinkedBag<T> temp = new LinkedBag<>();
+		Node otherData = other.firstNode;
+		
+		while(otherData != null) {
+			temp.add(otherData.data);
+			otherData = otherData.next;
+		}
+		
+		LinkedBag<T> result = new LinkedBag<>();
+		Node thisData = firstNode;
+	
+		while(!temp.isEmpty() && thisData != null) {
+			if(temp.remove(thisData.data)) {
+				result.add(thisData.data);
+			}
+			thisData = thisData.next;
+		}
+		
+		return result;
+	}
+	
+	private class Node {
+		private  T data;
+		private Node next;
+
+		Node ( T data, Node nextNode) {
+			this.data = data;
+			next = nextNode;
+		}
+
+		Node (T data) {
+			this (data, null);
+		}
+	}
+	
+	
+}
+
+
